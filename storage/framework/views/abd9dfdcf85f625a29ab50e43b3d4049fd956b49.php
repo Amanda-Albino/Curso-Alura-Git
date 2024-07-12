11d7139f9a3e2f95df0dd40fab21654d9813f1d8
Amanda Albino <amanda-albino@outlook.com>
2024-07-12 10:08:04 -0300

    Aprendendo Laravel

---
+++ storage/framework/views/abd9dfdcf85f625a29ab50e43b3d4049fd956b49.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo e($title); ?> - Controle de Series </title>
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+</head>
+<body>
+    <div class="container">
+    <h1><?php echo e($title); ?></h1>
+
+    <?php echo e($slot); ?>
+
+    </div>
+
+</body>
+</html><?php /**PATH C:\xampp\htdocs\teste\aprendendo1-laravel\resources\views/components/layout.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c6dd5c240c0227a857ad940e8a93c9952b779199.php
@@ -0,0 +1,23 @@
+<?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.layout','data' => ['title' => 'Nova Serie']]); ?>
+<?php $component->withName('layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['title' => 'Nova Serie']); ?>
+    <form action="/aprendendo1-laravel/public/series/salvar" method="post">
+        <?php echo csrf_field(); ?>
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome">
+        </div>
+
+        <button type="submit" class="btn btn-dark mb-2">Adicionar</button>
+        <a href="/aprendendo1-laravel/public/series"class="btn btn-dark mb-2">Voltar</a>
+
+    </form>
+  <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?><?php /**PATH C:\xampp\htdocs\teste\aprendendo1-laravel\resources\views/series/create.blade.php ENDPATH**/ ?>
